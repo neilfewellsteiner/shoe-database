95712aedb287f3459e05956b51f8d7e96cdf2384
root <root@ip-172-31-30-230.us-east-2.compute.internal>
2018-11-29 16:47:57 +0000

    Add Shoe

--- searchShoe.php
+++ searchShoe.php
@@ -1,20 +1,36 @@
 <?php
-	$sexL = $_POST['sexList'] .",";
-	$brandL = $_POST['brandList'] . ",";
-	$colorL = $_POST['colorList'] . ",";
-	$sizeL = $_POST['sizeL'] . ",";
-	$storeL = $_POST['storeL'] . ",";
+$servername = 'localhost:3306';
+$username = 'team7';
+$password = 'Team_7';
+$dbname = 'shoes';
 
-	// Open the text file
-	$file = fopen("searchResult.html", "w+");
-	ftruncate($file, 0); //Clear the file to 0bit
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 
-	$content = $sexL. $brandL. $colorL. $sizeL. $storeL;
-	// Write text
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 
-	// Close the text file
-	fwrite($file , $content); //Now lets write it in there
-	fclose($file );
+		$type   = strval($_POST["sexList"]);
+        $brand  = strval($_POST['brandList']);
+        $color  = strval($_POST["colorList"]);
+        $size   = strval($_POST["sizeList"]);
+        $store  = strval($_POST["storeList"]);
 
-    die(header("Location: ".$_SERVER["HTTP_REFERER"]));
+	$sql = "select * from shoe where brand_name='$brand' AND store_name='$store' AND size='$size' AND color='$color' AND type='$type'";
+
+	$result = mysqli_query($conn, $sql);
+
+	if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "size: " . $row["size"]. " - Brand: " . $row["brand_name"].   " - Store: " . $row["store_name"].  " - color: " . $row["color"]. " - Sex: " . $row["type"].  "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+mysqli_close($conn);
+
 ?>
