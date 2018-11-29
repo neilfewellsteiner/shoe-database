9aa90f434068222517db6c30abfb77315ffbc115
root <root@ip-172-31-30-230.us-east-2.compute.internal>
2018-11-29 19:50:34 +0000

    Search Done

--- searchShoe.php
+++ searchShoe.php
@@ -18,20 +18,33 @@ if (!$conn) {
         $size   = strval($_POST["sizeList"]);
         $store  = strval($_POST["storeList"]);
 
+	$whereVal = " where ";
 	$brandVal = "brand_name='$brand' ";
 	$storeVal = "AND store_name='$store' ";
+	$sizeVal  = "AND size='$size' ";
+	$colorVal = "AND color='$color' ";
+	$typeVal  = "AND type='$type' ";
 
-	$sql = "select * from shoe where ". $brandVal . $storeVal . "AND size='$size' AND color='$color' AND type='$type'";
+	if($brand=="blank"){$brandVal = ""; $storeVal = "store_name='$store' ";};
+	if($store=="blank"){$storeVal = ""; $sizeVal = "size='$size' ";};
+	if($size=="blank"){$sizeVal = ""; $colorVal = "color='$color' ";};
+	if($color=="blank"){$colorVal = ""; $typeVal = "type='$type' ";};
+	if($type=="blank"){$typeVal = "";};
+
+	if($brand=="blank" && $store=="blank" && $size=="blank" && $color=="blank" && $type=="blank")
+	{$whereVal = "";};
+
+	$sql = "select * from shoe" . $whereVal . $brandVal . $storeVal . $sizeVal . $colorVal . $typeVal;
 
 	$result = mysqli_query($conn, $sql);
 
 	if (mysqli_num_rows($result) > 0) {
     // output data of each row
     while($row = mysqli_fetch_assoc($result)) {
-        echo "size: " . $row["size"]. " - Brand: " . $row["brand_name"].   " - Store: " . $row["store_name"].  " - color: " . $row["color"]. " - Sex: " . $row["type"].  "<br>";
+        echo "size: " . $row["size"]. " - Brand: " . $row["brand_name"].   " - Store: " . $row["store_name"].  " - color: " . $row["color"]. " - Sex: " . $row["type"]. " - Price: ". $row["price"] . "<br>";
     }
 } else {
-    echo "0 results";
+    echo $sql;
 }
 
 mysqli_close($conn);
